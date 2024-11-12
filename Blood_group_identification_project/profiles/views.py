3c5e37bd2f94d8221921295590adf115963f702b
babith1234 <babithpoojari@gmail.com>
2024-11-12 17:43:31 +0530

    Image display partially done

--- Blood_group_identification_project/profiles/views.py
+++ Blood_group_identification_project/profiles/views.py
@@ -1,7 +1,22 @@
+# views.py
 from django.shortcuts import render
-
-# Create your views here.
+from django.core.files.storage import FileSystemStorage
 
 def profiles(request):
-    return render(request,'profiles.html')
-
+    if request.method == "POST" and request.FILES.get('abo_image'):
+        # Get the uploaded image
+        img_file = request.FILES['abo_image']
+        print(img_file)
+        
+        # Save the image to the media directory
+        fs = FileSystemStorage()
+        filename = fs.save(img_file.name, img_file)
+        
+        # Get the URL of the uploaded image
+        img_url = fs.url(filename)
+        print(img_url)
+        
+        # Pass the image URL to the template
+        return render(request, 'profiles.html', {'img_url': img_url})
+    
+    return render(request, 'profiles.html')
